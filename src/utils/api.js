377e4c09ac9f1de30300bc02ca2c29197f9dea96
kedycatsudo <dkocausta34@gmail.com>
2025-09-26 22:43:27 -0400

    baseurl, homepage,packagejson

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-const baseUrl = 'http://localhost:3001';
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://api.wtwrkedycat.baselinux.net' // <-- your deployed backend URL!
+    : 'http://localhost:3001';
 function checkResponse(res) {
   return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
 }
